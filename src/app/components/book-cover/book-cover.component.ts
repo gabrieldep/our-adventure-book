f5d509bba3e7c6de35bbae19fa305604ac42f932
Gabriel de Paula <gabriel.psilva@totvs.com.br>
2026-02-19 19:50:11 -0300

    Cover fill all the book

--- src/app/components/book-cover/book-cover.component.ts
+++ src/app/components/book-cover/book-cover.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-cover',
   standalone: true,
-  host: { class: 'block w-full h-full min-w-[320px] min-h-[280px]' },
+  host: { class: 'block w-full h-full min-w-0 min-h-0 flex-1' },
   templateUrl: './book-cover.component.html',
   styleUrl: './book-cover.component.css',
 })
