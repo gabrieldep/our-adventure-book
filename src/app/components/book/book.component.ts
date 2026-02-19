1cdb063dbd5557e5b894297c1619f6c5c0e3f32a
Gabriel de Paula <gabriel.psilva@totvs.com.br>
2026-02-19 11:33:59 -0300

    Revert "Tryng to flip pages"
    This reverts commit 00f6629d2486db5f2cf5be951ccdae2d27ebe4bf.

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,24 +1,14 @@
-import { Component, inject, signal, effect, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { NgComponentOutlet } from '@angular/common';
-import type { Type } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { AdventureService } from '../../services/adventure.service';
-import { FlipbookTriggerService } from '../../services/flipbook-trigger.service';
+import { AdventurePageComponent } from '../adventure-page/adventure-page.component';
 import { BookCoverComponent } from '../book-cover/book-cover.component';
 import { TimelineComponent } from '../timeline/timeline.component';
 import { AdminComponent } from '../admin/admin.component';
-import { AdventurePageComponent } from '../adventure-page/adventure-page.component';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [
-    BookCoverComponent,
-    TimelineComponent,
-    AdminComponent,
-    NgComponentOutlet,
-    AdventurePageComponent,
-  ],
+  imports: [AdventurePageComponent, BookCoverComponent, TimelineComponent, AdminComponent],
   template: `
     <div class="book-spread relative flex flex-col lg:flex-row gap-4 lg:gap-0 items-center justify-center min-h-screen paper-texture p-4">
       <button
@@ -46,27 +36,25 @@ import { AdventurePageComponent } from '../adventure-page/adventure-page.compone
           </div>
         </div>
       } @else {
-        @if (flipbookWrapperComponent()) {
-          <ng-container [ngComponentOutlet]="flipbookWrapperComponent()" />
-        } @else {
+        <div
+          class="spread flex flex-col lg:flex-row gap-4 lg:gap-0 w-full max-w-[95vw] lg:max-w-7xl xl:max-w-[1600px] items-stretch lg:items-stretch justify-center flex-1 lg:min-h-[80vh]"
+        >
           <div
-            class="spread flex flex-col lg:flex-row gap-4 lg:gap-0 w-full max-w-[95vw] lg:max-w-7xl xl:max-w-[1600px] items-stretch justify-center flex-1 lg:min-h-[80vh]"
+            class="page left-page flex-1 min-w-0 lg:min-w-[400px] lg:max-w-[50%] min-h-[60vh] lg:min-h-[80vh] paper-texture page"
           >
-            <div
-              class="page left-page flex-1 min-w-0 lg:min-w-[400px] lg:max-w-[50%] min-h-[60vh] lg:min-h-[80vh] paper-texture page"
-            >
-              <div class="p-6 lg:p-8 h-full">
-                <app-adventure-page />
-              </div>
-            </div>
-            <div class="spine hidden lg:block w-2 flex-shrink-0 bg-[#4B3621]/40 rounded mx-0.5" aria-hidden="true"></div>
-            <div
-              class="page right-page flex-1 min-w-0 lg:min-w-[400px] lg:max-w-[50%] min-h-[60vh] lg:min-h-[80vh] paper-texture flex items-center justify-center"
-            >
-              <div class="text-[#6b5344] font-typewriter text-sm">Next spread</div>
+            <div class="p-6 lg:p-8 h-full">
+              <app-adventure-page />
             </div>
           </div>
-        }
+
+          <div class="spine hidden lg:block w-2 flex-shrink-0 bg-[#4B3621]/40 rounded mx-0.5" aria-hidden="true"></div>
+
+          <div
+            class="page right-page flex-1 min-w-0 lg:min-w-[400px] lg:max-w-[50%] min-h-[60vh] lg:min-h-[80vh] paper-texture flex items-center justify-center"
+          >
+            <div class="text-[#6b5344] font-typewriter text-sm">Next spread</div>
+          </div>
+        </div>
       }
 
       <div class="flex gap-4 mt-6 lg:mt-8 lg:absolute lg:right-8 lg:top-1/2 lg:-translate-y-1/2 lg:flex-col">
@@ -91,38 +79,14 @@ import { AdventurePageComponent } from '../adventure-page/adventure-page.compone
   `,
 })
 export class BookComponent {
-  private readonly platformId = inject(PLATFORM_ID);
   readonly adventureService = inject(AdventureService);
-  private readonly flipbookTrigger = inject(FlipbookTriggerService);
   readonly showAdmin = signal(false);
 
-  /** Lazy-loaded only in the browser to avoid hammerjs/window on SSR. */
-  readonly flipbookWrapperComponent = signal<Type<unknown> | null>(null);
-
-  constructor() {
-    if (isPlatformBrowser(this.platformId)) {
-      effect(() => {
-        if (this.adventureService.isCoverPage()) return;
-        import('../adventure-flipbook-wrapper/adventure-flipbook-wrapper.component').then((m) =>
-          this.flipbookWrapperComponent.set(m.AdventureFlipbookWrapperComponent)
-        );
-      });
-    }
-  }
-
   goNext(): void {
-    if (this.adventureService.isCoverPage()) {
-      this.adventureService.nextPage();
-    } else {
-      this.flipbookTrigger.next.next();
-    }
+    this.adventureService.nextPage();
   }
 
   goPrev(): void {
-    if (this.adventureService.currentPageIndex() <= 1) {
-      this.adventureService.prevPage();
-    } else {
-      this.flipbookTrigger.prev.next();
-    }
+    this.adventureService.prevPage();
   }
 }
--- src/app/services/flipbook-trigger.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-
-/** Used by BookComponent to trigger flipbook next/prev without importing @labsforge/flipbook (SSR-safe). */
-@Injectable({ providedIn: 'root' })
-export class FlipbookTriggerService {
-  readonly next = new Subject<void>();
-  readonly prev = new Subject<void>();
-}
